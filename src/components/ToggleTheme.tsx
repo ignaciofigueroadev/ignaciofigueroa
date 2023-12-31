8adedfbcd3bfd217830104c0cb6ac573dbbcf815
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2023-12-31 19:41:19 -0300

    feat: adding the project page base

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 
-export function ToggleTheme() {
+interface ToggleThemeProps {
+  className?: string;
+}
+
+export function ToggleTheme({className}: ToggleThemeProps) {
   const getInitialTheme = () => {
     const userPrefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
+      "(prefers-color-scheme: dark)",
     ).matches;
     return (
       localStorage.getItem("theme") || (userPrefersDark ? "dark" : "light")
@@ -11,7 +15,6 @@ export function ToggleTheme() {
   };
 
   const [theme, setTheme] = useState(getInitialTheme);
-
   useEffect(() => {
     const userPrefersDark = window.matchMedia("(prefers-color-scheme: dark)");
     const updateTheme = () => {
@@ -27,19 +30,17 @@ export function ToggleTheme() {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [theme]);
-
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
-
   return (
     <button
       onClick={toggleTheme}
-      className={`rounded-lg flex justify-center items-center ${
+      className={`flex justify-center items-center ${
         theme === "dark"
           ? "bg-gradient-to-r from-amber-200 to-yellow-500"
           : "bg-[#000]"
-      } hover:scale-105 duration-200 cursor-default md:col-span-1 md:row-span-1 animate-fade animate-duration-[1500ms] animate-normal`}
+      } hover:scale-105 duration-200 cursor-default md:col-span-1 md:row-span-1 animate-fade animate-duration-[1500ms] animate-normal ${className}`}
     >
       <img
         src={theme === "dark" ? "/icons/sun.svg" : "/icons/moon.svg"}
