68b22f461b092cfdce29a45689709ae575d59131
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2024-01-22 17:20:51 -0300

    feat: adding bg color to the toggle theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -36,9 +36,7 @@ export function ToggleTheme({ className }: ToggleThemeProps) {
   return (
     <button
       onClick={toggleTheme}
-      className={`flex justify-center items-center p-[1rem]  ${
-        theme === "dark" ? "border-white border-2" : "border-black border-2"
-      } hover:scale-105 duration-200 cursor-default md:col-span-1 md:row-span-1 animate-fade animate-duration-[1500ms] animate-normal ${className}`}
+      className={`flex justify-center items-center p-[1rem] bg-gradient-to-r from-red-500 to-orange-500  hover:scale-105 duration-200 cursor-default md:col-span-1 md:row-span-1 animate-fade animate-duration-[1500ms] animate-normal ${className}`}
     >
       <img
         src={theme === "dark" ? "/icons/sun.svg" : "/icons/moon.svg"}
